3f1e7b24cc7a755d4c680235ca91c66a79c97c0c
Thanh Hieu Tran <ttran40@huskers.unl.edu>
2021-08-27 04:07:43 +0000

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Thanh Hieu Tran
+ *         ttran40@huskers.unl.edu
+ * Date: 2021/08/26
+ *
+ * First hack session with printing Name, and Major
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Thanh Hieu Tran!\n");
+  printf("My major is Computer Science.\n");
+
+  return 0;
+}
