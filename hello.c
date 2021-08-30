79f76ee34dc849b372055f21ee5af31d35e7de04
Thanh Hieu Tran <ttran40@huskers.unl.edu>
2021-08-30 00:54:05 +0000

    test

--- hello.c
+++ hello.c
@@ -1,4 +1,4 @@
-/**
+/
  * Author: Thanh Hieu Tran
  *         ttran40@huskers.unl.edu
  * Date: 2021/08/26
@@ -9,7 +9,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main(int argc, char **argv) {
+int main(int argc, char argv) {
 
   printf("Thanh Hieu Tran!\n");
   printf("My major is Computer Science.\n");
